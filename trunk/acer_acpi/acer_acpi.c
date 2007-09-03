770886c8cb5b9836387cdca83f789e75ba6b6681
cathectic <cathectic@33a1d4c2-402b-0410-8da8-5f0a6282fcb0>
2007-09-03 13:29:16 +0000

    Fix segfault on older kernels when trying to read brightness from backlight class

--- trunk/acer_acpi/acer_acpi.c
+++ trunk/acer_acpi/acer_acpi.c
@@ -1245,8 +1245,6 @@ static int __init acer_backlight_init(struct device *dev)
 	acer_backlight_device = bd;
 
 	bd->props->max_brightness = ACER_MAX_BRIGHTNESS;
-	bd->props->get_brightness = read_brightness(NULL);
-	/*update_bl_status(bd);*/
 	return 0;
 }
 #else
